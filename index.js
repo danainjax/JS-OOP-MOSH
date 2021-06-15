0dc9034f1630fdb8301245bbcf97733f1ccfe545
danainjax <danainjax@gmail.com>
2021-06-15 16:03:54 -0400

    constructors

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log('Hello Dana, OOP and away we go!')
+// console.log('Hello Dana')
+
+// let circle = {
+//     radius: 1,
+//     location: {
+//         x: 1,
+//         y: 1
+//     },
+//     draw () {
+//         console.log('draw');
+//     }
+// };
+
+// console.log(circle.radius)
+// console.log(circle.location)
+function createCircle(radius) {
+    return {
+        radius,
+        draw: function() {
+            console.log('draw');
+        }
+    };
+}
+
+
+const circle = createCircle();
+
+function Circle(radius) {
+    
+    this.radius = radius;
+    this.draw = function() {
+        console.log('draw');
+    }
+}
+
+const another = new Circle(1);
