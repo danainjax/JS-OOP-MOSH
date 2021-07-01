d875c001edfbfe0b0fe7f177c808b351eb3bb671
danainjax <danainjax@gmail.com>
2021-06-30 20:37:27 -0400

    cookie, taco, cheeseburger factories

--- index.js
+++ index.js
@@ -13,24 +13,65 @@
 
 // console.log(circle.radius)
 // console.log(circle.location)
-function createCircle(radius) {
-    return {
-        radius,
-        draw: function() {
-            console.log('draw');
-        }
-    };
+// f
+
+const circle = {
+    radius: 1,
+    location: {
+        x:1,
+        y:1
+    
+}
 }
 
+//object literal syntax
 
-const circle = createCircle();
-
-function Circle(radius) {
-    
-    this.radius = radius;
-    this.draw = function() {
-        console.log('draw');
+function createCheeseburger(bun, doneness,cheese){
+    return {
+        bun,
+        doneness,
+        cheese,
+        eat: function(){
+            console.log("I ate the cheeseburger!")
+        }
+        
     }
 }
 
-const another = new Circle(1);
+const cheeseburger = createCheeseburger('sesame-poppy', 'rare', 'muenster')
+console.log(cheeseburger)
+cheeseburger.eat()
+
+//if object has one or more methods, it has behavior
+//use a factory or constructor function
+
+function Taco(protein, shell) {
+    this.protein = protein;
+    this.shell = shell;
+    this.eat = function() {
+        console.log('I ate the tasty taco');
+    }
+}
+const firstTaco = new Taco('shredded beef', 'soft')
+console.log(firstTaco)
+firstTaco.eat()
+
+function Cookie(type) {
+    this.type = type;
+    this.dunk = function() {
+        console.log(`Dunk the ${type} cookie, and say ahh!`)
+    }
+}
+const chocolateChip = new Cookie('chocolate chip')
+console.log(chocolateChip)
+chocolateChip.dunk()
+
+function Order(name, email, phone, pizza_ids) {
+    this.name = name;
+    this.email = email;
+    this.phone = phone;
+    this.pizza_ids = pizza_ids
+}
+
+const order1 = new Order("Dana P.", "danainjax@gmail.com", "904-654-2981", [160,161,164])
+console.log(order1)
